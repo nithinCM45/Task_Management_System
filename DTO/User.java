2f968963d2b1f82622067bf0948bed90ef62c66b
nithinCM45 <137485506+nithinCM45@users.noreply.github.com>
2023-06-24 15:29:22 +0530

    Create User.java

---
+++ DTO/User.java
@@ -0,0 +1,28 @@
+package DTO;
+
+public class User {
+    private String username;
+    private String password;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    // Getters and setters to initialize and access private members
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
